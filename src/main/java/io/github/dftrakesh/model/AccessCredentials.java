081aea4869c6a7ed0e8b4238220cf05b23331a3b
nehagandhi24 <neha.gandhi@dakshyafreondtech.com>
2024-02-05 12:35:17 +0530

    Changes made in AccessCredentials

--- src/main/java/io/github/dftrakesh/model/AccessCredentials.java
+++ src/main/java/io/github/dftrakesh/model/AccessCredentials.java
@@ -1,57 +1,16 @@
 package io.github.dftrakesh.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AccessCredentials {
 
-    @JsonProperty("account_id")
     private Integer accountId;
-
-    @JsonProperty("storeName")
     private String storeName;
-
-    @JsonProperty("storeDomain")
-    private String storeDomain;
-
-    @JsonProperty("client_id")
     private String clientId;
-
-    @JsonProperty("client_secret")
     private String clientSecret;
-
-    @JsonProperty("access_token")
     private String accessToken;
-
-    @JsonProperty("token_type")
-    private String tokenType;
-
-    @JsonProperty("scope")
-    private String scope;
-
-    @JsonProperty("store_id")
     private String storeId;
-
-    @JsonProperty("user_id")
-    private long userId;
-
-    @JsonProperty("admin_sso")
-    private AdminSSO adminSSO;
-
-    @JsonProperty("email")
-    private String email;
-
-    @JsonProperty("profile_scopes")
-    private String profileScopes;
-
-    @JsonProperty("public_token")
-    private String publicToken;
-
-    public static class AdminSSO {
-        @JsonProperty("role")
-        private String role;
-    }
 }
